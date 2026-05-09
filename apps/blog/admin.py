12e8d1da9e2c2a16afb90081df483d033164123f
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-09 08:24:16 +0300

    feat: зарегистраровано django_mptt_admin, изменён DraggableMPTTAdmin на DjangoMpttAdmin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from .models import Post, Category
 
 from mptt.admin import DraggableMPTTAdmin
+from django_mptt_admin.admin import DjangoMpttAdmin 
 # Register your models here.
 
 admin.site.register(Post)
 
 @admin.register(Category)
-class CategoryAdmin(DraggableMPTTAdmin):
+class CategoryAdmin(DjangoMpttAdmin):
     """
     Админ панель модели категорий
     """

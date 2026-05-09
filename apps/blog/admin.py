bbfd16d5291731cc8a9fcffe7e53d4ccec2032ce
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-09 08:05:54 +0300

    feat: зарегестрирована модель category с возможностью скрывать категории по вложенности

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
-from models import Post
+from models import Post, Category
+
+from mptt.admin import DraggableMPTTAdmin
 # Register your models here.
 
 admin.site.register(Post)
+
+@admin.register(Category)
+class CategoryAdmin(DraggableMPTTAdmin):
+    """
+    Админ панель модели категорий
+    """
+    prepopulated_fields = {'slug':('title',)}

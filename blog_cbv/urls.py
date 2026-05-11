3094842debc337c1cd2c5cdbaa32c007ce60db1b
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-11 08:02:09 +0300

    fix: namespace should be in include()

--- blog_cbv/urls.py
+++ blog_cbv/urls.py
@@ -21,8 +21,8 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.blog.urls'), namespace = 'blog'),
+    path('', include('apps.blog.urls', namespace = 'blog')),
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, doculent_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

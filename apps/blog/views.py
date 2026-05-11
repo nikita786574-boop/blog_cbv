caee26d7720e3a13c248f1fc378e86969f1fc249
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-11 07:23:28 +0300

    feat: PostListView в views

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
+
+# Представление для вывода списка статей
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
 
 # Create your views here.

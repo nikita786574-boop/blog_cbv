b441b1c0ff13a9fcf5ef53a733674bd9d2c38c44
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-11 08:11:02 +0300

    feat: передаётся доп контекст title, переопредлён метод get_context_data в PostListView

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -9,5 +9,10 @@ class PostListView(ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        return context
+
 
 # Create your views here.

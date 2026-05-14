bf931db3fc43f916ea8d79343cc7618eba69876f
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-14 07:20:03 +0300

    feat: detail view для Post

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
+from django.views.generic import DetailView
 
 # Представление для вывода списка статей
 
@@ -16,3 +17,13 @@ class PostListView(ListView):
 
 
 # Create your views here.
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = self.object.title
+        return context

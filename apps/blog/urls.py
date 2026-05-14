bde57a1b9140448832ff6c53c643fa2b0db965e1
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-14 07:20:21 +0300

    feat: url для PostDetail

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'blog'
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='home')
+    path('', views.PostListView.as_view(), name='home'),
+    path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
 ]

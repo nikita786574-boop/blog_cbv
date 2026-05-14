4ca102527eaffd5cd17a54ee0ee1cb7d1e85b010
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-14 07:49:49 +0300

    feat: ф-ция для герерации уникальных слагов

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -5,7 +5,13 @@ from mptt.admin import DraggableMPTTAdmin
 from django_mptt_admin.admin import DjangoMpttAdmin 
 # Register your models here.
 
-admin.site.register(Post)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """
+    Админ-панель модели записи
+    """
+    prepopulated_fields = {'slug':('title',)}
+
 
 @admin.register(Category)
 class CategoryAdmin(DjangoMpttAdmin):

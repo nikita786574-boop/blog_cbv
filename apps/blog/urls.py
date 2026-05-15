3caf5721e5dfc9bd43ac8b889e33092de5242e97
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-15 07:43:33 +0300

    feat: url для PostFromCategory

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('', views.PostListView.as_view(), name='home'),
     path('post/<slug:slug>/', views.PostDetailView.as_view(), name='post_detail'),
+    path('category/<slug:slug>/', views.PostFromCategory.as_view(), name = 'post_by_category')
 ]

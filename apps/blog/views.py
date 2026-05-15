389220072195066d5a603ee21fb217857680bbcc
Прищепа Никита Иванович <niprishchepa@edu.hse.ru>
2026-05-15 08:00:25 +0300

    feat: пагинация PostListView

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -9,10 +9,13 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    paginate_by = 2
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Главная страница'
+        page = context['page_obj']
+        context['paginator_range'] = page.paginator.get_elided_page_range(page.number)
         return context
 
 
